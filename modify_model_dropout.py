0831a0b9b1f9fa48149536dbee1c90ffefdbf82f
kouyoumin <kouyoumin@gmail.com>
2024-09-06 09:54:28 +0800

    Error handling

--- modify_model_dropout.py
+++ modify_model_dropout.py
@@ -2,14 +2,21 @@ import torch.nn as nn
 
 
 def modify_last_dropout_rate(model, new_drop_rate=-1):
+    drop_layer = None
     for n, m in model.named_modules():
         if isinstance(m, nn.modules.dropout._DropoutNd):
             print('Found dropout layer (%s): %s' % (n, m))
             drop_layer = m
+    
+    if drop_layer is None:
+        print('No dropout layer found')
+        return 0.
    
     if new_drop_rate >= 0:
-        print(f'Set new drop rate to {new_drop_rate}')
+        print(f'Set drop rate to {new_drop_rate}')
         drop_layer.p = new_drop_rate
+    else:
+        print(f'Got drop rate {drop_layer.p}')
         
     return drop_layer.p
 
