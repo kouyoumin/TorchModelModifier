3497886c069b11e4718e9c2ac36513a9c34c1879
kouyoumin <kouyoumin@gmail.com>
2024-11-06 11:47:07 +0800

    Refactor

--- modify_model_dropout.py
+++ modify_model_dropout.py
@@ -1,12 +1,28 @@
 import torch.nn as nn
 
 
-def modify_last_dropout_rate(model, new_drop_rate=-1):
+def _find_dropout_layer(model):
     drop_layer = None
     for n, m in model.named_modules():
         if isinstance(m, nn.modules.dropout._DropoutNd):
             print('Found dropout layer (%s): %s' % (n, m))
             drop_layer = m
+    
+    return drop_layer
+
+
+def get_last_dropout_rate(model):
+    drop_layer = _find_dropout_layer(model)
+    
+    if drop_layer is None:
+        print('No dropout layer found')
+        return 0.
+   
+    return drop_layer.p
+
+
+def modify_last_dropout_rate(model, new_drop_rate=-1):
+    drop_layer = _find_dropout_layer(model)
     
     if drop_layer is None:
         print('No dropout layer found')
@@ -15,8 +31,6 @@ def modify_last_dropout_rate(model, new_drop_rate=-1):
     if new_drop_rate >= 0:
         print(f'Set drop rate to {new_drop_rate}')
         drop_layer.p = new_drop_rate
-    else:
-        print(f'Got drop rate {drop_layer.p}')
         
     return drop_layer.p
 
